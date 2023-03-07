9db6d1182be5c23c0f8cc824d4df92cc8298c892
Guillaume Bonjean <gbonjean@free.fr>
2023-03-07 19:42:48 +0100

    modifs seeds

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :gym
-  # belongs_to :owner, class_name: "User"
-  belongs_to :user
+  belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
 
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings

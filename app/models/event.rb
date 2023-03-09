b1cb60e11d7efc90ef390b4d3e47683906ffef88
Guillaume Bonjean <gbonjean@free.fr>
2023-03-09 12:25:38 +0100

    back new create ok

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,4 @@
 class Event < ApplicationRecord
-  attr_accessor :date, :start_time, :end_time
-
   belongs_to :gym
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
 
@@ -11,6 +9,4 @@ class Event < ApplicationRecord
   has_many :messages, through: :chatroom
 
   validates :owner, :start_time, :end_time, :slots, :description, presence: true
-
-
 end

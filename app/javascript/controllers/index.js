c2de1291e81a6c3b02175d490f5a72d3713c5db6
Paulin Bougault-Bourges <paulin.bougault@yahoo.fr>
2023-03-10 11:39:32 +0100

    flatpickr tout casser

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,6 +7,9 @@ import { application } from "./application"
 import AftereffectController from "./aftereffect_controller"
 application.register("aftereffect", AftereffectController)
 
+import FlatpickrController from "./flatpickr_controller"
+application.register("flatpickr", FlatpickrController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
